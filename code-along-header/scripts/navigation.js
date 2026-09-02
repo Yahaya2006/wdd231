a60c75925ef3351198e4b11dd9b3a01ec969a419
Yahaya2006 <diaminata2006@gmail.com>
2026-09-02 07:15:59 +0100

    Add initial project files and styles
    - Created index.html with basic structure and linked stylesheets.
    - Added navigation and footer sections with social media links.
    - Implemented responsive design with base.css and larger.css.
    - Included normalize.css for consistent styling across browsers.
    - Added JavaScript for navigation toggle functionality.
    - Introduced SVG icons for GitHub, Instagram, and LinkedIn.

---
+++ code-along-header/scripts/navigation.js
@@ -0,0 +1,14 @@
+const navbutton = document.querySelector('#ham-btn');
+const navBar = document.getElementById("nav-bar");
+
+const currentYear = new Date().getFullYear();
+document.getElementById("current-year").textContent = currentYear;
+
+const lastModified = document.getElementById("last-modified");
+lastModified.textContent = document.lastModified;
+
+navbutton.addEventListener('click', () => {
+     navbutton.classList.toggle('show');
+     navBar.classList.toggle('show');
+});
+
