32789c402be534e06ded3f853b4f6b8bacf547f8
Yahaya2006 <diaminata2006@gmail.com>
2026-09-07 18:27:30 +0100

    Add meta tags for SEO and social sharing; create prophets page with styling and script

---
+++ scripts/prophets.js
@@ -0,0 +1,37 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+const cards = document.getElementById("cards");
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    // console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+
+getProphetData();
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        const card = document.createElement('section');
+        const fullName = document.createElement('h2');
+        const portrait = document.createElement('img');
+        const paragraph = document.createElement('p');
+
+        paragraph.textContent = `Date of Birth: ${prophet.birthdate},
+                                Place of Birth : ${prophet.birthplace}`;
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height','440');
+
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+        card.appendChild(paragraph);
+
+        cards.appendChild(card);
+    });
+}
